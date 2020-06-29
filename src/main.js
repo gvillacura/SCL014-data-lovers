80680f57063baf792aaf1f57c6782119c33c4315
Geraldine Villacura R <geraldinevillacuraruz@gmail.com>
2020-06-29 17:20:50 -0400

    obtención nombre y fotos de página inicial

--- src/main.js
+++ src/main.js
@@ -1,6 +1,26 @@
-import { example } from './data.js';
-// import data from './data/atletas/atletas.js';
-// import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
+import {
+  example
+} from './data.js';
+import data from './data/potter/potter.js';
 
-console.log(example, data);
+console.log(data);
+
+const container = document.querySelector('#cartas');
+
+data.forEach((persona) => {
+  // Div principal de tarjeta
+  const personaElement = document.createElement('article');
+  personaElement.classList.add('information');
+
+  const personaNombreElement = document.createElement('div');
+  personaNombreElement.innerHTML = `${persona.name}`;
+  personaNombreElement.classList.add('nombre-persona');
+
+  const fotoElement = document.createElement('img');
+  fotoElement.src = persona.image;
+  fotoElement.classList.add('foto-persona');
+
+  personaElement.append(personaNombreElement);
+  container.append(personaElement);
+  personaElement.append(fotoElement);
+});
